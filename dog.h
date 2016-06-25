4dd1c0ad3d39e9b3ef62cbf3905cb2f4b79c2399
qjx <qjx>
2016-06-25 14:16:42 +0800

    添加dog类

---
+++ dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  AFNetworkingDemo
+//
+//  Created by 权建星 on 16/6/25.
+//  Copyright © 2016年 huangyibiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ dog.m
@@ -0,0 +1,14 @@
+
+//
+//  dog.m
+//  AFNetworkingDemo
+//
+//  Created by 权建星 on 16/6/25.
+//  Copyright © 2016年 huangyibiao. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
